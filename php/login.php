aceb5f7eef1d39f71b6a65779109c14b44ce844f
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-09-13 15:18:56 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -15,6 +15,9 @@ $loginData = json_decode(file_get_contents($loginDataPath), true);
 $USERNAME = $loginData['username'];
 $PASSWORD = $loginData['password'];
 
+echo $USERNAME;
+echo $PASSWORD;
+
 # check if the user is authenticated using server authentication
 if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
     if ($_SERVER['PHP_AUTH_USER'] === $USERNAME && $_SERVER['PHP_AUTH_PW'] === $PASSWORD) {
